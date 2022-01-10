89e5dbe925f7a6381c58ef309a63f97da4de39cb
João Paulo <joaopaulomorais2@gmail.com>
2022-01-10 15:25:01 -0300

    add JOptionPane

--- src/calculadora/SimplesCalculadora.java
+++ src/calculadora/SimplesCalculadora.java
@@ -1,38 +1,46 @@
 package calculadora;
 
+import javax.swing.JOptionPane;
+
 public class SimplesCalculadora {
 
 	public static void main(String[] args) {
 		boolean sair = false;
 		int opcao = 1;
+		double numero1;
+		double numero2;
 
 		do {
-			System.out.println("Escolha uma opção");
-			System.out.println("1 - Somar");
-			System.out.println("2 - Subtrair");
-			System.out.println("3 - Multiplicar");
-			System.out.println("4 - Dividir");
+			
+			JOptionPane.showInputDialog(
+					  "Escolha uma opção\n"
+					+ "1 - Somar\n"
+					+ "2 - Subtrair\n"
+					+ "3 - Multiplicar\n"
+					+ "4 - Dividir\n"
+			);
 
 			switch (opcao) {
 				case 1:
-					System.out.println("Otimo, vamos somar dois números");
+					
 					break;
 	
 				case 2:
-					System.out.println("Otimo, vamos subtrair dois números");
+					
 					break;
 	
 				case 3:
-					System.out.println("Otimo, vamos multiplicar dois números");
+					
 					break;
 	
 				case 4:
-					System.out.println("Otimo, vamos dividir dois números");
+					
 					break;
 	
 				default:
 					break;
 			}
 		} while (sair);
+				
 	}
 }
